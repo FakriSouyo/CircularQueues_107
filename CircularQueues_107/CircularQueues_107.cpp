f58b37c8b78f2214bf9446803bc9a7203a0db09a
Fakhri abdillah <fakriggwp@gmail.com>
2023-06-23 09:43:25 +0700

    mencetak semua elemen dalam antrean (queue) yang terdapat dalam array dari posisi FRONT hingga posisi REAR.

--- CircularQueues_107/CircularQueues_107.cpp
+++ CircularQueues_107/CircularQueues_107.cpp
@@ -87,3 +87,13 @@ public:
 				}
 
 				FRONT_position = 0;
+
+				// literasi dari awal array hingga REAR
+				while (FRONT_position <= REAR_position) {
+					cout << queve_array[FRONT_position] << "   ";
+					FRONT_position++;
+				}
+				cout << endl;
+			}
+		}
+	};
